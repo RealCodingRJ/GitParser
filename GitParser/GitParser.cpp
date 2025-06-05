5fe8f2358b8ed2adfaad4907eca7ba85f4e68e3e
Ryan James <realcoder2023@gmail.com>
2025-06-05 12:21:23 -0400

    Add project files.

---
+++ GitParser/GitParser.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <Windows.h>
+
+
+static void createCommand(std::string gitCommand) {
+
+	ShellExecuteA(nullptr, "open", gitCommand.c_str(), nullptr, nullptr, SW_SHOWDEFAULT);
+
+}
+
+void cloneProject(const std::string clonedProject) {
+
+	ShellExecuteA(nullptr, "open", clonedProject.c_str(), nullptr, nullptr, SW_SHOWDEFAULT);
+
+}
+
+static void createUsersInput(std::string inputMessage) {
+
+	std::cin >> inputMessage;
+}
+
+struct ClonedProd {
+
+	std::string prod;
+};
+
+struct CommandRoute{
+
+	std::string route;
+};
+
+int main() {
+
+
+	CommandRoute route;
+	ClonedProd prod;
+
+	createUsersInput(route.route);
+
+	if (route.route == "add") {
+		createCommand("git add .");
+	}
+
+	if (route.route == "commit") {
+		createCommand("git commit -m added Code");
+	}
+
+	if (route.route == "Clone") {
+		std::cout << "Enter URL: " << std::endl;
+
+		std::cin >> prod.prod;
+		cloneProject("git clone " + prod.prod);
+	}
+
+	if (route.route == "All") {
+
+		createCommand("git add -A");
+		createCommand("git commit -m added code to route");
+		createCommand("git push -u origin main");
+
+	}
+
+
+	createCommand(route.route);
+
+
+}
